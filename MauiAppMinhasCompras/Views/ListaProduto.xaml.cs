fb34f746877681c17370b3ab935cdf9e7fbd9644
RobsonBPA <robsonpasetto1705@gmail.com>
2026-03-07 11:59:39 -0300

    Adicionado o sistema de cadastrar um novo produto.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,4 +6,17 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+            // Redireciona o usuário para a página de cadastro de produto
+            Navigation.PushAsync(new Views.NovoProduto());
+		} catch (Exception ex)
+		{
+            // Mensagem de erro
+            DisplayAlertAsync("Ops", ex.Message, "OK");
+		}
+    }
 }
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,27 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				// Obtém os valores inseridos pelo usuario
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			// Mensagem de sucesso
+			await DisplayAlertAsync("Sucesso!", "Registro inserido", "OK");
+
+		} catch (Exception ex)
+		{
+			// Mensagem de erro
+			await DisplayAlertAsync("Ops", ex.Message, "OK");
+		}
+    }
 }
