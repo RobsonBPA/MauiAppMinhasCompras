3a910f49f6322565731afae3ae6ac552cbafd5dc
RobsonBPA <robsonpasetto1705@gmail.com>
2026-03-21 07:27:17 -0300

    Inserida a validação de dados para cadastrar produtos.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,52 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        String _descricao;
+        double _quantidade;
+        double _preco;
+
+        String msg = "Por favor, preencha";
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception($"{msg} a descrição.");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception($"{msg} a quantidade.");
+                }
+
+            _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception($"{msg} o preço.");
+                }
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
